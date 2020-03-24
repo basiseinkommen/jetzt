e923cac8baba66e89ecdbb77f4beb36faeab48c0
speexy <jennifer.meyer@posteo.de>
2020-03-24 19:11:26 +0100

    move navbar and footer to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import Layout from '@common/Layout';
-import Navbar from '@common/Navbar';
-
 import Header from '@sections/Header';
 import About from '@sections/About';
 import Brands from '@sections/Brands';
@@ -12,13 +10,11 @@ import Footer from '@sections/Footer';
 
 const IndexPage = () => (
   <Layout>
-    <Navbar />
     <Header />
     <About />
     {/* <Brands />
     <Team />
     <Faq /> */}
-    <Footer />
   </Layout>
 );
 
