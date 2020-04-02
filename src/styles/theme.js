0cecea9ecfdd24ccf9de152d0c925bd14e3c5997
Timo Uhlmann <timouhlmann@gmail.com>
2020-04-02 15:56:45 +0000

    Reduce font size, add illustrations

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,11 +6,11 @@ const theme = {
     secondary: `'Arial', sans-serif`
   },
   font_size: {
-    small: 'font-size: 20px; line-height: 30px',
-    regular: 'font-size: 24px; line-height: 32px',
-    large: 'font-size: 30px; line-height: 40px',
-    larger: 'font-size: 36px; line-height: 48px',
-    xlarge: 'font-size: 48px; line-height: 56px'
+    small: 'font-size: 16px; line-height: 24px',
+    regular: 'font-size: 20px; line-height: 30px',
+    large: 'font-size: 24px; line-height: 32px',
+    larger: 'font-size: 30px; line-height: 40px',
+    xlarge: 'font-size: 36px; line-height: 48px'
   },
   color: {
     white: {
